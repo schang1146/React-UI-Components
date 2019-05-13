7470d7fa93c0167e258d4a3b759695194d2bffd5
schang1146 <sammychang2185@gmail.com>
2019-05-12 23:16:25 -0700

    redoing to include props

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -7,4 +7,28 @@ const ActionButton = (props) => {
     );
 };
 
+const ButtonClear = {
+    value: 'clear',
+};
+
+const ButtonDivide = {
+    value: 'clear',
+};
+
+const ButtonTimes = {
+    value: 'clear',
+};
+
+const ButtonMinus = {
+    value: 'clear',
+};
+
+const ButtonPlus = {
+    value: 'clear',
+};
+
+const ButtonEqual = {
+    value: 'clear',
+};
+
 export default ActionButton;
