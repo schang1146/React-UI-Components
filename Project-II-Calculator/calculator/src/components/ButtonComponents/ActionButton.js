a239e486378f20b5a8b3499b7d0200b37afc4f9a
schang1146 <sammychang2185@gmail.com>
2019-05-12 17:45:02 -0700

    create initial layout of calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (button) => {
+    return (
+        <button className='btn btn-act'>{button.action}</button>
+    );
+};
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (button) => {
+    return (
+        <button className='btn btn-num'>{button.value}</button>
+    );
+};
+
+export default NumberButton;
